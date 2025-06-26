e8525ace449d17b82ba146d478c24b6c2c8287be
Ephson G <leocorp96@gmail.com>
2025-06-26 22:11:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# streamlit-joystick
+
+This component allows you to have a joystick in streamlit
+
+## Installation instructions 
+
+```sh
+pip install streamlit-joystick
+```
+
+## Usage instructions
+
+```python
+import streamlit as st
+
+from st_joystick import st_joystick
+
+value = st_joystick()
+
+st.write(value)
---
+++ setup.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-joystick",
+    version="0.1.0",
+    author="Ephson Guakro",
+    author_email="leocorp96@gmail.com",
+    description="This component allows you to have a joystick in streamlit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=["streamlit>=1.2", "jinja2"],
+)
